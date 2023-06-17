0530ec779b0b2721c18a4faf47ca94343ffddc61
Lazar Antonijevic <lazar.antonijevic@appollo41.com>
2023-06-17 20:21:50 +0200

    Error handling added

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connectDB');
 
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Ad Poster API');
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 
---
+++ middleware/error-handler.js
@@ -0,0 +1,12 @@
+const { CustomError } = require('../errors/custom-error');
+const { StatusCodes } = require('http-status-codes');
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+};
+
+module.exports = errorHandlerMiddleware;
