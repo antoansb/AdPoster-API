c1d97d378e3b5462ac67a1983e464df38c35918b
Lazar Antonijevic <lazar.antonijevic@appollo41.com>
2023-06-18 18:10:53 +0200

    Adding JWT

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -1,11 +1,14 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
 
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { username: user.username }, token });
 };
 
 const login = async (req, res) => {
